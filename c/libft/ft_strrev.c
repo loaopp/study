342a74afe7d974255bcb857572e9c8914e151a0d
jeon <test@naver.com>
2020-11-06 23:59:48 +0900

    crlf to lf

--- c/libft/ft_strrev.c
+++ c/libft/ft_strrev.c
@@ -6,27 +6,27 @@
 /*   By: yejeon <yejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 23:50:35 by yejeon            #+#    #+#             */
-/*   Updated: 2020/11/03 23:50:35 by yejeon           ###   ########.fr       */
+/*   Updated: 2020/11/06 23:57:39 by yejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strrev(char *s)
+char	*ft_strrev(char *s)
 {
-    char    *p1;
-    char    *p2;
-    char    ch;
+	char	*p1;
+	char	*p2;
+	char	ch;
 
-    p1 = s;
-    p2 = s + ft_strlen(s) - 1;
-    while (p1 < p2)
-    {
-        ch = *p1;
-        *p1 = *p2;
-        *p2 = ch;
-        p1++;
-        p2--;
-    }
-    return (s);
+	p1 = s;
+	p2 = s + ft_strlen(s) - 1;
+	while (p1 < p2)
+	{
+		ch = *p1;
+		*p1 = *p2;
+		*p2 = ch;
+		p1++;
+		p2--;
+	}
+	return (s);
 }
